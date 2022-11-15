26e3871afb89c303416c9964306949811bd84839
Jisheng Dai <jsdai.ustc@gmail.com>
2022-11-15 16:03:26 +0800

    Update main.m

--- main.m
+++ main.m
@@ -7,11 +7,13 @@ N_point=200; % number of grid points
 SNR=10;
 Ps=sqrt((   10.^(SNR/10)   )/2);
 X=Ps*( randn(T,N)+1i*randn(T,N)  );
-DOA=(randn(1,L)-0.5)*2*90;
+DOA=(rand(1,L)-0.5)*2*90;
 A = exp(-1i*pi*(0:N-1)'*sind(DOA));
 H=A*(randn(L,1)+1i*randn(L,1));
 noise=sqrt(1/2)*(randn(T,1)+1i*randn(T,1));
 Y=X*H + noise;
 etc=T;       % or etc=length(DOA) if the number of DoAs is known
+
+%% Proposed method
 H_est=offgrid_Bayesian_ULA(Y,X,N,N_point,etc);
-norm(H-H_est,'fro')^2/norm(H)^2
+norm(H-H_est,'fro')^2/norm(H,'fro')^2
